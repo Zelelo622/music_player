4424fc146faae81cef5d7bacb3bb6d02559ce86d
Zelelo622 <egorcom27@gmail.com>
2024-10-03 00:58:21 +0300

    init

---
+++ src/App.tsx
@@ -0,0 +1,8 @@
+import { PrismaneProvider } from "@prismane/core";
+import React from "react";
+
+const App: React.FC = (): JSX.Element => (
+  <PrismaneProvider>HelloWorld</PrismaneProvider>
+);
+
+export default App;
